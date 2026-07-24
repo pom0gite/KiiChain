48f393d3ae6eab442fd35f453d0068218bf194cc
pom0gite <98287395+pom0gite@users.noreply.github.com>
2026-07-24 15:38:38 +0300

    Update Greeting.sol

--- Greeting.sol
+++ Greeting.sol
@@ -1,1 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
+contract Greeting {
+
+    string public greeting = "Hello KiiChain!";
+
+    function setGreeting(string memory newGreeting) public {
+        greeting = newGreeting;
+    }
+}

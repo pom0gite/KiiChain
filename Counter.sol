c070dfb4a57fea9a090e0a28c33726e9bd6fd6fd
pom0gite <98287395+pom0gite@users.noreply.github.com>
2026-07-24 03:01:55 +0300

    Update Counter.sol

--- Counter.sol
+++ Counter.sol
@@ -1,1 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
+contract Counter {
+
+    uint256 public count;
+
+    function increment() public {
+        count++;
+    }
+
+    function decrement() public {
+        require(count > 0, "Counter is zero");
+        count--;
+    }
+
+    function reset() public {
+        count = 0;
+    }
+}

1177ef5424a275d4f6e1cdd5d9697a91d46602db
pom0gite <98287395+pom0gite@users.noreply.github.com>
2026-07-25 04:07:34 +0300

    Update MessageBoard.sol

--- MessageBoard.sol
+++ MessageBoard.sol
@@ -1,1 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
+contract MessageBoard {
+
+    string public message;
+
+    constructor() {
+        message = "Welcome to KiiChain";
+    }
+
+    function updateMessage(string memory newMessage) public {
+        message = newMessage;
+    }
+}

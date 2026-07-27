23efc755d75afd204b9300a5766e95e59ad6ca75
pom0gite <98287395+pom0gite@users.noreply.github.com>
2026-07-27 22:49:15 +0300

    Update SimpleVault.sol

--- SimpleVault.sol
+++ SimpleVault.sol
@@ -1,1 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
+contract SimpleVault {
+
+    mapping(address => uint256) public deposits;
+
+    function recordDeposit(uint256 amount) public {
+        deposits[msg.sender] += amount;
+    }
+
+    function getDeposit(address user) public view returns(uint256) {
+        return deposits[user];
+    }
+}
